096e18c137a86f562f36732c5fd15562f66cf59e
gobbedy <gperr050@uottawa.ca>
2021-03-10 15:03:44 -0500

    create base.html and reddit.html

--- data_visualization/urls.py
+++ data_visualization/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='dashboard-home'),
-    #path('reddit/',views.reddit, name='dashboard-reddit'),
+    path('', views.index, name='home'),
+    path('reddit/', views.reddit, name='reddit'),
     #path('movers/',views.movers, name='dashboard-movers'),
     #path('yahoo_movers/', views.max_gainer, name='dashboard-movers'),
     #path('<str:ticker>/', views.get_details_ticker, name='dashboard-ticker'),
--- data_visualization/views.py
+++ data_visualization/views.py
@@ -3,3 +3,6 @@ from django.http import JsonResponse
 
 def index(request):
 	return render(request, 'data_visualization/index.html')
+
+def reddit(request):
+	return render(request, 'data_visualization/reddit.html')
